ed75bd34d23c38fcf42e2ea808934e9587ebcf7f
rayan bch <bouchenakrayan@gmail.com>
2024-09-07 21:28:00 +0100

    final commit

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -6,37 +6,35 @@ import lampe1 from "../assets/images/lampe1.png";
 import lampe2 from "../assets/images/lampe2.png";
 
 const Home = ({ onShowPages }) => {
-  const [isLampeOn, setIsLampeOn] = useState(true); 
+  const [isLampeOn, setIsLampeOn] = useState(true);
 
   const handleShowAllPages = () => {
     onShowPages();
   };
 
   const toggleLampe = () => {
-    setIsLampeOn((prev) => !prev); 
+    setIsLampeOn((prev) => !prev);
   };
 
   return (
     <div className="w-full h-[100vh] relative">
-      
       <div className="absolute top-0 right-0 max-lg:hidden">
         <img
-          src={isLampeOn ? lampe1 : lampe2} 
+          src={isLampeOn ? lampe1 : lampe2}
           alt="Lampe"
-          className="mr-[200px] cursor-pointer"
-          onClick={toggleLampe} 
+          className="mr-[10vw] cursor-pointer"
+          onClick={toggleLampe}
         />
       </div>
-      <div className="flex flex-col justify-center items-center gap-6">
-
-        <div className="flex flex-row gap-16 font-bold text-3xl items-center justify-center pt-[50px] max-lg:text-lg">
+      <div className="flex flex-col justify-center items-center ">
+        <div className="flex flex-row gap-[8%] font-bold text-[1.5vw] items-center justify-center pt-[2%] pb-[1%] max-lg:text-[3vw] max-sm:pt-[15%] max-sm:text-[4vw]">
           <p>
             <a
               href="#about"
               onClick={handleShowAllPages}
               className="hover:text-[#B5C18E] transition-colors duration-300"
             >
-              About
+              Education
             </a>
           </p>
           <p>
@@ -62,45 +60,45 @@ const Home = ({ onShowPages }) => {
           <img
             src={etageres}
             alt="Shelves"
-            className="w-[600px] max-lg:w-[500px]"
+            className="w-[30%] max-lg:w-[80%] max-sm:w-[80%]"
           />
         </div>
       </div>
-      <div className="flex w-full justify-center text-center pt-16 max-lg:justify-center max-lg:p-0 max-xl:p-0 max-xl:justify-center ">
-        <p className="font-bold text-[#914F1E] text-[90px] max-lg:text-[50px]">
+      <div className="flex w-full justify-center text-center pt-[1%] max-lg:justify-center  max-sm:pt-[20%]">
+        <p className="font-bold text-[#914F1E] text-[5vw] max-lg:text-[6vw]">
           <span className="typewriter">
             Hey There,<br />
             I'm <span className="text-[#B5C18E]">Rayane</span>
           </span>
         </p>
       </div>
-      <div className="flex justify-center w-full text-center max-xl:p-0 max-xl:justify-center max-lg:justify-center max-lg:p-0">
+      <div className="flex justify-center w-full text-center mt-[1%]">
         <div className="w-fit">
-          <p className="font-extralight text-[40px] text-[#986438] max-lg:text-[30px]">
+          <p className="font-extralight text-[2vw] text-[#986438] max-lg:text-[5vw]">
             I design beautiful websites,<br />
             and I love coding them.
           </p>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center w-full pt-24 max-lg:pt-10 relative z-20 max-lg:hidden">
+      <div className="flex flex-col justify-center items-center w-full mt-[5%] relative z-20 max-lg:pt-[3%] max-lg:hidden">
         <a href="#about">
           <img
             src={doublearrow}
             alt="Double Arrow"
-            className="animate-bounce hover:scale-150 max-lg:w-[30px]"
+            className="animate-bounce hover:scale-150 max-lg:w-[15%]"
             onClick={onShowPages}
             style={{ cursor: "pointer" }}
           />
         </a>
-        <p className="font-thin text-lg text-[#986438] mt-4 max-lg:hidden max-sm:hidden">
+        <p className="font-thin text-[1.2vw] text-[#986438] mt-[1%] max-lg:hidden max-sm:hidden">
           Click to learn more
         </p>
       </div>
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 max-lg:flex max-lg:justify-center w-full z-10">
+      <div className="absolute bottom-0 left-1/2  -translate-x-1/2 max-lg:flex max-lg:justify-center w-full z-10">
         <img
           src={rayaneicon}
           alt="Rayane Icon"
-          className="xl:w-[450px] lg:w-[400px]"
+          className="h-[50vh]"
         />
       </div>
     </div>
